336fcf6243b3d46fde736dd7d5e9925cf72a1e2a
Ernests Dejus <ernisd621@gmail.com>
2024-01-31 13:44:28 +0200

    React on steroids

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,12 @@ import Log from "./components/Log.jsx";
 import GameOver from "./components/GameOver.jsx";
 import { WINNING_COMBINATIONS } from "./winning-combinations.js";
 
+const PLAYERS = {
+  X: "Player 1",
+  O: "Player 2"
+}
 
-const initialGameBoard = [
+const INITIAL_GAME_BOARD = [
   [null, null, null],
   [null, null, null],
   [null, null, null],
@@ -23,17 +27,8 @@ function deriveActivePlayer(gameTurns){
   return currentPlayer;
 }
 
-function App() {
-  const [players, setPlayers] = useState({
-    "X" : "Player 1",
-    "O" : "Player 2",
-  });
-  const [gameTurns, setGameTurns] = useState([])
-  // const [hasWinner, setHasWinner] = useState(false);
-  // const [activePlayer, setActivePlayer] = useState("X");
-  const activePlayer = deriveActivePlayer(gameTurns);
-
-  let gameBoard = [...initialGameBoard.map(array => [...array])];
+function deriveGameBoard(gameTurns){
+  let gameBoard = [...INITIAL_GAME_BOARD.map(array => [...array])];
 
   for(const turn of gameTurns){
       const{square, player} = turn;
@@ -41,6 +36,10 @@ function App() {
 
       gameBoard[row][col] = player;
   }
+  return gameBoard;
+}
+
+function deriveWinner(gameBoard,players){
   let winner;
 
   for(const combination of WINNING_COMBINATIONS){
@@ -55,6 +54,18 @@ function App() {
     }
   }
 
+  return winner;
+}
+
+function App() {
+
+  const [players, setPlayers] = useState(PLAYERS);
+  const [gameTurns, setGameTurns] = useState([])
+  // const [hasWinner, setHasWinner] = useState(false);
+  // const [activePlayer, setActivePlayer] = useState("X");
+  const activePlayer = deriveActivePlayer(gameTurns);
+  const gameBoard = deriveGameBoard(gameTurns);
+  const winner = deriveWinner(gameBoard, players);
   const hasDraw = gameTurns.length === 9 && !winner;
 
   function handleSelectedSquare(rowIndex , colIndex){
@@ -88,12 +99,12 @@ function App() {
       <div id="game-container">
         <ol id="players" className="highlight-player">
             <Player 
-            initialName = "Player 1" 
+            initialName = {PLAYERS.X} 
             symbol="X"
             onChangeName={handlePlayerNameChange}
             isActive={activePlayer === "X"} />
             <Player
-            initialName = "Player 2" 
+            initialName = {PLAYERS.O}
             symbol="O"
             onChangeName={handlePlayerNameChange}
             isActive={activePlayer === "O"} />
